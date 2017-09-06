c492e984fd8735edd09c3aa7f371841e15f232ab
Mitchell Cohen <mitchell@agilebits.com>
2017-09-06 11:31:16 -0400

    removed dist

--- dist/es6/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { Plural, generator } from "./helpers";
-export { default as T, makeT } from "./t-i18n";
--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var helpers_1 = require("./helpers");
-exports.Plural = helpers_1.Plural;
-exports.generator = helpers_1.generator;
-var t_i18n_1 = require("./t-i18n");
-exports.T = t_i18n_1.default;
-exports.makeT = t_i18n_1.makeT;
